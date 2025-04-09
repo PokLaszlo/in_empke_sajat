2d1abce4114453753d3765a791ffa2bb09bc2b98
Pók László <pokica6@gmail.com>
2025-04-09 12:07:56 +0200

    Create művelet kész

--- web/src/app.js
+++ web/src/app.js
@@ -1,19 +1,27 @@
 const tbody = document.querySelector('#tbody')
+const saveButton = document.getElementById("saveButton")
+const nameInput = document.querySelector("#name")
+const idInput = document.querySelector("#id")
+const cityInput = document.querySelector("#city")
+const salaryInput = document.querySelector("#salary")
 
 //Read
 const url = 'http://localhost:8000/api/employees'
 
 //Promise
 
-fetch(url)
-.then((response) => {
-  return response.json()
-})
-.then((result) => {
-  console.log(result)  
-  renderTbody(result.data)
-});
+function getEmployees(){
+  fetch(url)
+  .then((response) => {
+    return response.json()
+  })
+  .then((result) => {
+    renderTbody(result.data)
+  });
+}
 
+
+getEmployees()
 function renderTbody(empList) {
   var tbodyContent = '';
   empList.forEach((emp) => {
@@ -30,4 +38,39 @@ function renderTbody(empList) {
   tbody.innerHTML = tbodyContent
   
 
+}
+//create művelet
+
+saveButton.addEventListener("click",()=>{
+  //javascript objektum
+  const emp  = {
+    name:nameInput.value,
+    city:cityInput.value,
+    salary:salaryInput.value
+  }
+  addEmployee(emp)
+  clearFields()
+})
+
+function clearFields(){
+  nameInput.value="";
+  idInput.value="";
+  cityInput.value="";
+  salaryInput.value="";
+}
+
+function addEmployee(empData){
+  fetch(url,{
+    method: 'POST',
+    body: JSON.stringify(empData),
+    headers:{
+      "Content-Type":"application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(result=>{
+    console.log(result),
+    getEmployees()
+  })
+  .catch(err =>console.log(err))
 }
